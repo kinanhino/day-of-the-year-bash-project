d22a526a3ef59488313bc3ae2e9be6b3389b89be
kinanhino <kinanhino24@gmail.com>
2023-08-21 21:48:11 +0300

    fixed typo + make the code more readable

--- main.sh
+++ main.sh
@@ -1,76 +1,83 @@
 #! /bin/bash
 
+re='^[0-9]+$'
+
+
+
 #Ella
 ###################################################################
 #check parameters
+leap_year () {
+	if [[ "$#" -ne 1 ]] || [[ ! $1 =~ ^[0-9]+$ ]]; then
+	echo "Invalid numbers of arguments"
 
-if [[ "$#" -ne 1 ]] || [[ ! $1 =~ ^[0-9]+$ ]]; then
-echo "Invalid numbers of arguments"
+	exit 1
+	fi
 
-exit 1
-fi
+		year=$1
 
-        year=$1
+	#check if the ##year## is bigger than 1582
 
-#check if the ##year## is bigger than 1582
-
-        if [ $year -lt 1582 ]; then 
-        echo "Not withing the Gregorian calendar period"
-        exit 1
-        fi
+	if [ $year -lt 1582 ]; then 
+		echo "Not within the Gregorian calendar period"
+		exit 1
+	fi
 
 
-#check if the year number is divisible 4
-        if [ $(($year % 4)) -ne 0 ]; then
-        echo "Common year"
-        
-#check if the ##year## number divisible 100
-        elif [ $(($year % 100)) -ne 0 ]; then
-        echo  "Leap year"
-        
-#check if the ##year## number divisble 400
-        elif [ $(($year % 400)) -ne 0 ]; then
-        echo "Common year"
+	#check if the year number is divisible 4
+	if [ $(($year % 4)) -ne 0 ]; then
+		echo "Common year"
+		
+	#check if the ##year## number divisible 100
+	elif [ $(($year % 100)) -ne 0 ]; then
+		echo  "Leap year"
+		
+	#check if the ##year## number divisble 400
+	elif [ $(($year % 400)) -ne 0 ]; then
+		echo "Common year"
 
-        else echo  "Leap year"
-        fi 
-
+	else 
+		echo  "Leap year"
+	fi 
+}
 ###############################################################
-re='^[0-9]+$'
+
 # a function to calculate the number of the day in a given year (out of 365 or 366)
 day_of_the_year () { # $1 = year; $2 = month; $3 = day
-
-if [[ "$#" -ne 3 ]]; then
-	echo "Error: Invalid number of arguments (must be 3)" >&2
-	exit 1
-elif [[ ! "$1" =~ $re ]]; then
-	echo "Error: $1 is not a valid year"
-	exit 1
-elif [[ ! "$2" =~ $re ]]; then
-	echo "Error: $2 is not a valid month"
-	exit 1
-elif [[ ! "$3" =~ $re ]]; then
-        echo "Error: $3 is not a valid day"
-        exit 1
-else
-	counter=0
-	year=$1
-	month=$2
-	day=$3
-	for (( i=1; i<$month; i++ )); do
-		days_in_month=$(number_of_days_in_month $year $month)
-		let counter=counter+days_in_month
-	done
-	
-	let counter=$counter+$day
-	echo $counter
-fi
+	if [[ "$#" -ne 3 ]]; then
+		echo "Error: Invalid number of arguments (must be 3)" >&2
+		exit 1
+	elif [[ ! "$1" =~ $re ]]; then
+		echo "Error: $1 is not a valid year"
+		exit 1
+	elif [[ ! "$2" =~ $re ]]; then
+		echo "Error: $2 is not a valid month"
+		exit 1
+	elif [[ ! "$3" =~ $re ]]; then
+		echo "Error: $3 is not a valid day"
+		exit 1
+	else
+		counter=0
+		year=$1
+		month=$2
+		day=$3
+		for (( i=1; i<$month; i++ )); do
+			#echo $i
+			days_in_month=$(month_in_year $year $i)
+			#echo "days param: $days_in_month"
+			counter=$((counter + days_in_month))
+		done
+		counter=$((counter + day))
+		is_leap=$(leap_year $year)
+		if [[ "$is_leap" == "Leap year" ]]; then
+    			total_days="366"
+		else
+    			total_days="365"
+		fi
+		echo -n "the $counter"; echo "th out of $total_days"
+	fi
 
 }
-
-
-
-
 
 #start main
 read -p "enter the year, month and day seperated by space : " -a calendar
